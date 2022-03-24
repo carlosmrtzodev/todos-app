d3c7ae83541daf50e583d17873298f64bee0a1b2
DNC\carlo <carlosmrtzow8@gmail.com>
2022-03-24 16:54:47 -0400

    setup folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import App from "./App";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
