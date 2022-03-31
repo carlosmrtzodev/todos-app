a29ec5ce674539360b71b68039b01d322ccebb96
DNC\carlo <carlosmrtzow8@gmail.com>
2022-03-31 13:09:19 -0400

    update

--- src/components/tasks.jsx
+++ src/components/tasks.jsx
@@ -7,7 +7,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTask, getTasks } from "../actions/tasks";
 
-const tasks = () => {
+const Tasks = () => {
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks);
   const [currentTask, setCurrentTask] = useState(null);
